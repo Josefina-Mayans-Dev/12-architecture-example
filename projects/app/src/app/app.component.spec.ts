ed6c0a650ce5ebaee310bcfe33d04b6f4a703b90
JacoboGarcesO <jacobogarcesoquendo@gmail.com>
2025-01-29 09:29:52 -0500

    Tests

--- projects/app/src/app/app.component.spec.ts
+++ projects/app/src/app/app.component.spec.ts
@@ -8,22 +8,48 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
+  it('should be 5', () => {
+    const num1 = 3;
+    const num2 = 2;
+
+    const result = num1 + num2;
+
+    expect(result).toBe(5);
+  });
+
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'app' title`, () => {
+  it('should render router-outlet', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('app');
+    const compiled = fixture.nativeElement as HTMLElement;
+
+    expect(compiled.querySelector('router-outlet')).toBeTruthy();
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
+    fixture.componentRef.setInput('title', 'Angular');
+
     fixture.detectChanges();
+    const app = fixture.componentInstance;
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, app');
+    expect(app.title()).toBe('Angular');
+    expect(compiled.querySelector('p').textContent).toBe('Angular');
+  })
+
+  it('should be called onClick', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    const compiled = fixture.nativeElement as HTMLElement;
+    const buttonElement = compiled.querySelector('button');
+    spyOn(app.onClick, 'emit');
+
+    buttonElement.click();
+    
+    expect(app.onClick.emit).toHaveBeenCalled();
   });
 });
--- projects/app/src/app/app.component.ts
+++ projects/app/src/app/app.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  template: '<router-outlet />',
+  template: '<router-outlet /> <p>{{title()}}</p> <button (click)="handleClick()">Click me</button>',
 })
-export class AppComponent { }
+export class AppComponent {
+  title = input<string>();
+  onClick = output<void>();
+
+  handleClick() {
+    this.onClick.emit();
+  }
+}
